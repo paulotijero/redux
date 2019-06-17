d15a5435bcb90af7b748b8b2cb5e7e5c450d1dee
paulotijero <paulotijero@gmail.com>
2019-06-17 17:59:25 -0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+/** @jsx jsx */
+import React from "react";
+import { render } from "react-dom";
+import { jsx, Global } from "@emotion/core";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const global = {
+  body: {
+    margin: 0,
+    fontFamily: "Roboto, sans-serif",
+    background: "#f2f3f5",
+    color: "#34495e",
+    fontSize: "15px"
+  }
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function App() {
+  return (
+    <>
+      <Global styles={global} />
+    </>
+  );
+}
+
+render(<App />, document.getElementById("root"));
