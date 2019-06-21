f687c0cb60ddee31460de13dc44692f6c4b4d955
paulotijero <paulotijero@gmail.com>
2019-06-21 10:35:41 -0500

    Add plus

--- src/views/cart.js
+++ src/views/cart.js
@@ -3,9 +3,20 @@ import { jsx } from "@emotion/core";
 import React from "react";
 import { useCartProducts, useTotal } from "../selectors";
 
+import { useAddProduct } from "../action-hooks";
+
 function Cart() {
   const products = useCartProducts();
   const total = useTotal();
+  const addProduct = useAddProduct();
+
+  function handleAddProductClick(event) {
+    addProduct(
+      products.find(
+        product => product.id === parseInt(event.target.dataset.product, 10)
+      )
+    );
+  }
 
   return (
     <>
@@ -24,6 +35,14 @@ function Cart() {
               <tr>
                 <td>{product.name}</td>
                 <td>{product.price}</td>
+                <td>
+                  <button
+                    onClick={handleAddProductClick}
+                    data-product={product.id}
+                  >
+                    +
+                  </button>
+                </td>
                 <td>{product.quantity}</td>
                 <td>{product.price * product.quantity}</td>
               </tr>
