341c96861fd0914b05be82c817a0c4c43eeae608
paulotijero <paulotijero@gmail.com>
2019-06-21 11:43:06 -0500

    Use connect

--- src/views/cart.js
+++ src/views/cart.js
@@ -1,14 +1,18 @@
 /** @jsx jsx */
 import { jsx } from "@emotion/core";
 import React from "react";
+import { connect } from "react-redux";
 import { useCartProducts, useTotal } from "../selectors";
+import { addProduct } from "../actions";
 
-import { useAddProduct } from "../action-hooks";
+function getTotal(products) {
+  return products.reduce((a, b) => {
+    return a + b.quantity * b.price;
+  }, 0);
+}
 
-function Cart() {
-  const products = useCartProducts();
-  const total = useTotal();
-  const addProduct = useAddProduct();
+function Cart({ products, addProduct }) {
+  const total = getTotal(products);
 
   function handleAddProductClick(event) {
     addProduct(
@@ -57,4 +61,21 @@ function Cart() {
   );
 }
 
-export default Cart;
+function mapState(state) {
+  return {
+    products: Object.values(state.cart)
+  };
+}
+
+function mapDispatch(dispatch) {
+  return {
+    addProduct(product) {
+      return dispatch(addProduct(product));
+    }
+  };
+}
+
+export default connect(
+  mapState,
+  mapDispatch
+)(Cart);
